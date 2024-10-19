2a4df2e8083f2322f447d5990c6c38052cebb32e
ern23zc <U202112907@upc.edu.pe>
2024-10-19 11:40:47 -0500

    fix: Toolbar display for mobile devices

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+function openNav() {
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mobile-menu").style.width = "0%";
+}
+
 // Function to smoothly scroll to the targeted section
 function smoothScroll(target) {
     document.getElementById(target).scrollIntoView({
